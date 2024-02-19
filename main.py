b8cfcec060f02f6248c71f90eb09b43b393c4fc7
AnandC7github <122742595+AnandC7github@users.noreply.github.com>
2024-02-19 06:41:27 +0000

    Add about_page route to the Flask application

--- main.py
+++ main.py
@@ -7,4 +7,8 @@ app = Flask('app')
 def hello_world():
   return '<h1>Changed Text!!!</h1>'
 
+@app.route('/about')
+def about_page():
+  return '<h1>About Page</h1>'
+
 app.run(host='0.0.0.0', port=8080)
