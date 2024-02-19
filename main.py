59f8bbd10f1493376207f273913335ddf96e79d3
AnandC7github <122742595+AnandC7github@users.noreply.github.com>
2024-02-19 06:37:28 +0000

    new edit in main.py line 8

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ app = Flask('app')
 
 @app.route('/')         # python decorators - What url in your website i am going to navigate through
 def hello_world():
-  return 'Hello, World!'
+  return '<h1>Hello, World, but bigger!</h1>'
 
 app.run(host='0.0.0.0', port=8080)
