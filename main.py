f9e0dafe83fa33eebdbf9791302b2175507dc76c
AnandC7github <122742595+AnandC7github@users.noreply.github.com>
2024-02-19 06:54:15 +0000

    Update main.py to render 'home.html' using Flask's render_template

--- main.py
+++ main.py
@@ -1,14 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask('app')
 
 
 @app.route('/')         # python decorators - What url in your website i am going to navigate through
-def hello_world():
-  return '<h1>Changed Text!!!</h1>'
-
-@app.route('/about/<username>')
-def about_page(username):
-  return f'<h1>This is the about page of {username} </h1>'
+def home_page():
+  return render_template('home.html')
 
 app.run(host='0.0.0.0', port=8080)
