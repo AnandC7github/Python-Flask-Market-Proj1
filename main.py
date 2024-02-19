17cf90e8caa81c442a83467631d0be83243296be
AnandC7github <122742595+AnandC7github@users.noreply.github.com>
2024-02-19 06:46:10 +0000

    Add dynamic username parameter to the /about route in the Flask application

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def hello_world():
   return '<h1>Changed Text!!!</h1>'
 
 @app.route('/about/<username>')
-def about_page():
+def about_page(username):
   return f'<h1>This is the about page of {username} </h1>'
 
 app.run(host='0.0.0.0', port=8080)
