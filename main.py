2ae65cfc0946f7cc7cf952461350ae980b8db281
AnandC7github <122742595+AnandC7github@users.noreply.github.com>
2024-02-20 12:21:22 +0000

    Moved the Item model and associated database configuration to a separate 'models.py' file for better project organization and separation of concerns.

--- main.py
+++ main.py
@@ -5,19 +5,6 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
 db = SQLAlchemy(app)
 
-
-class Item(db.Model):
-  """Database table for ITEMS"""
-  id = db.Column(db.Integer, primary_key=True)
-  name = db.Column(db.String(30), nullable=False)
-  barcode = db.Column(db.String(), nullable=False)
-  price = db.Column(db.Integer(), nullable=False)
-  description = db.Column(db.String(), nullable=False)
-
-  def __repr__(self):
-    return f'Item {self.name}'
-
-
 @app.route(
     '/'
 )  # python decorators - What url in your website i am going to navigate through
---
+++ models.py
@@ -0,0 +1,10 @@
+class Item(db.Model):
+  """Database table for ITEMS"""
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String(30), nullable=False)
+  barcode = db.Column(db.String(), nullable=False)
+  price = db.Column(db.Integer(), nullable=False)
+  description = db.Column(db.String(), nullable=False)
+
+  def __repr__(self):
+    return f'Item {self.name}'
