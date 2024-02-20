07c28bffbbadec6b4fe91b07bbf752efd1ecb3b6
AnandC7github <122742595+AnandC7github@users.noreply.github.com>
2024-02-20 05:32:52 +0000

    Add route for '/market'

--- main.py
+++ main.py
@@ -8,4 +8,8 @@ app = Flask('app')
 def home_page():
   return render_template('home.html')
 
+@app.route('/market')
+def market_page():
+  return render_template('market.html')
+
 app.run(host='0.0.0.0', port=8080, debug= True)
