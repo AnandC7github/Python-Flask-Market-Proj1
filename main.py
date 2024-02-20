3d141c9c66f26202c78046e3cc5245b8b0e75b96
AnandC7github <122742595+AnandC7github@users.noreply.github.com>
2024-02-20 09:40:05 +0000

    revert back the testing

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask('app')
 
 
 @app.route('/')         # python decorators - What url in your website i am going to navigate through
-@app.route('/homehomehome')
+@app.route('/home')
 def home_page():
   return render_template('home.html')
 
