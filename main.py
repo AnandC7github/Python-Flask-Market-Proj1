70334fabaff825b501a2604e7df30786c48c0850
AnandC7github <122742595+AnandC7github@users.noreply.github.com>
2024-02-20 11:21:54 +0000

    changed due to error

--- main.py
+++ main.py
@@ -7,11 +7,15 @@ db = SQLAlchemy(app)
 
 
 class Item(db.Model):
+  """Database table for ITEMS"""
   id = db.Column(db.Integer, primary_key=True)
-  name = db.Column(db.String(length=100), nullable=False)
+  name = db.Column(db.String(30), nullable=False)
+  barcode = db.Column(db.String(), nullable=False)
   price = db.Column(db.Integer(), nullable=False)
-  barcode = db.Column(db.String(length=12), unique=True, nullable=False)
-  description = db.Column(db.String(length=2500), unique=True, nullable=False)
+  description = db.Column(db.String(), nullable=False)
+
+  def __repr__(self) -> str:
+    return f"ITEM < {self.name} | ${self.price} >"
 
 
 @app.route(
@@ -44,10 +48,49 @@ def market_page():
 
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=3000, debug=True)
 
-  #DATA-BASE
+  #DATA-BASE CODE HERE --------------------------
   with app.app_context():
+    # # drop the database tables
+    db.drop_all()
+    # # create database and table
     db.create_all()
+    print(">> Database :: <<")
+    # # Add an item into the ITEM table
+    item = Item()
+    item.name = 'Phone X'
+    item.barcode = '893212299889'
+    item.price = 600
+    item.description = 'Coolest phone X ever'
 
-  
+    db.session.add(item)
+    db.session.commit()
+
+    # Different Syntax ---
+    item_1 = Item(name='Phone',
+                  barcode='893212299897',
+                  price=500,
+                  description='Coolest phone ever')
+    item_2 = Item(name='Laptop',
+                  barcode='993212299897',
+                  price=1500,
+                  description='Coolest Laptop ever')
+    item_3 = Item(name='Apple Laptop',
+                  barcode='993212299999',
+                  price=2500,
+                  description='Coolest Apple Laptop ever')
+    item_4 = Item(name='Gameboy',
+                  barcode='993212299897',
+                  price=150,
+                  description='Coolest Gameboy ever')
+    db.session.add_all([item_1, item_2, item_3, item_4])
+    db.session.commit()
+
+    # # print all the items in the table ITEM
+    # print(Item.query.all())
+    for item in Item.query.all():
+      print(">> ", item)
+
+  # -------------------------------------------------------------
+
+  app.run(host='0.0.0.0', port=3000, debug=True)
