d2fbb814f500508aeb32cbb3f1d709878c4a1984
AnandC7github <122742595+AnandC7github@users.noreply.github.com>
2024-02-21 07:16:10 +0000

    checking for error...duplicated the file 'routes.py' inside of market folder and changed the routes import in __init__.py

--- market/__init__.py
+++ market/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
-from market import routes
+from routes import app as routes_app
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
