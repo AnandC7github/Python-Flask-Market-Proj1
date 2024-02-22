e289efba17737591f14930263583e675306dfdb7
Anand C <122742595+AnandC7github@users.noreply.github.com>
2024-02-22 10:17:27 +0000

    deleting duplicate items

--- main.py
+++ main.py
@@ -3,5 +3,15 @@ from market.models import Item, User
 
 
 if __name__ == '__main__':
+  with app.app_context():
+    # Remove item with ID 2
+    item_to_remove_2 = Item.query.get(2)
+    db.session.delete(item_to_remove_2)
+
+    # Remove item with ID 5
+    item_to_remove_5 = Item.query.get(5)
+    db.session.delete(item_to_remove_5)
+
+    db.session.commit()
   print(__name__)
   app.run(host="0.0.0.0", port=3000, debug=True)
