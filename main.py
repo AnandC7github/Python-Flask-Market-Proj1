cddc7bdf444a5fe242bf166b401364fe428647aa
Anand C <122742595+AnandC7github@users.noreply.github.com>
2024-02-22 10:23:55 +0000

    updated the id of some items

--- main.py
+++ main.py
@@ -3,17 +3,15 @@ from market.models import Item, User
 
 
 if __name__ == '__main__':
-  with app.app_context():
-  # Change ID of item 3 to 2
-    item_to_update_3 = Item.query.get(3)
-    if item_to_update_3:
-        item_to_update_3.id = 2
+  # with app.app_context():
+  #   # Remove item with ID 2
+  #   item_to_remove_2 = Item.query.get(2)
+  #   db.session.delete(item_to_remove_2)
 
-  # Change ID of item 4 to 3
-    item_to_update_4 = Item.query.get(4)
-    if item_to_update_4:
-      item_to_update_4.id = 3
+  #   # Remove item with ID 5
+  #   item_to_remove_5 = Item.query.get(5)
+  #   db.session.delete(item_to_remove_5)
 
-  db.session.commit()
+  #   db.session.commit()
   print(__name__)
   app.run(host="0.0.0.0", port=3000, debug=True)
