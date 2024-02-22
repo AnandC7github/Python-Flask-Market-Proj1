db2df2fbe7ac0ffdf31c02eceb1332584bf80799
Anand C <122742595+AnandC7github@users.noreply.github.com>
2024-02-22 06:05:49 +0000

    code to create a table

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-from market import app
+from market import app, db
 
 #checks if the run.py has executed directly and not imported 
 if __name__ == '__main__':
+  with app.app_context():
+  # Create the tables
+    db.create_all()
   print(__name__)
   app.run(host="0.0.0.0", port=3000, debug=True)
