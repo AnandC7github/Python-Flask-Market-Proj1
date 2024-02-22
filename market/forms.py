64031fd1bc34372769ba0b69121a4b59bf6c68b0
Anand C <122742595+AnandC7github@users.noreply.github.com>
2024-02-22 11:19:13 +0000

    Added RegisterForm class to forms.py with fields for 'username' and 'email_address'

--- market/forms.py
+++ market/forms.py
@@ -1,3 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField
 
+class RegisterForm(FlaskForm):
+  username = StringField(label = 'username')
+  email_address = StringField(label = 'email')
+  
