532994edcbb909137bfce1dc62aace021322e822
Anand C <122742595+AnandC7github@users.noreply.github.com>
2024-02-22 10:40:55 +0000

    added the owner/user id

--- main.py
+++ main.py
@@ -3,5 +3,11 @@ from market.models import Item, User
 
 
 if __name__ == '__main__':
-  print(__name__)
-  app.run(host="0.0.0.0", port=3000, debug=True)
+  with app.app_context():
+  # Update owner/user_id for items in the database
+    items_to_update = Item.query.all()
+    for item in items_to_update:
+      item.owner = 1
+    db.session.commit()
+    print(__name__)
+    app.run(host="0.0.0.0", port=3000, debug=True)
