e3e0a9e2e1e17dd3b004ca73e0c157de02059f07
Anand C <122742595+AnandC7github@users.noreply.github.com>
2024-02-22 10:11:25 +0000

    added another item

--- main.py
+++ main.py
@@ -3,11 +3,14 @@ from market.models import Item, User
 
 if __name__ == '__main__':
     with app.app_context():
-        item_to_update = Item.query.get(2)
-
-        items_to_delete = Item.query.filter(Item.id.between(2, 14)).all()
-        for item in items_to_delete:
-            db.session.delete(item)
+        # Adding a new item - Office Chair
+        office_chair = Item(
+            name='Office Chair',
+            barcode='96573029468394',
+            price=25000,
+            description='A comfortable and ergonomic office chair designed to provide optimal support and style for your workspace. This chair features: - High-quality materials for durability and longevity. - Adjustable seat height and armrests for personalized comfort. - Lumbar support to maintain a healthy posture during long hours of work. - Smooth swivel and casters for easy mobility. - Stylish design to enhance the aesthetics of your office.'
+        )
+        db.session.add(office_chair)
         db.session.commit()
 
         print(__name__)
