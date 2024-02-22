c77aedec0f635b0258cb6780e63f802917762794
Anand C <122742595+AnandC7github@users.noreply.github.com>
2024-02-22 09:09:00 +0000

    added code for recreating the tables after dropping them

--- main.py
+++ main.py
@@ -4,5 +4,10 @@ from market.models import Item
 
 #checks if the run.py has executed directly and not imported 
 if __name__ == '__main__':
+  with app.app_context():
+    # drop the database tables
+    db.drop_all()
+    # create database and table
+    db.create_all()
     print(__name__)
     app.run(host="0.0.0.0", port=3000, debug=True)
