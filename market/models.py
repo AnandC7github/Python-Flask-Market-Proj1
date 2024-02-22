12159c85ab73726f0c8224e15a368ebdd8073768
Anand C <122742595+AnandC7github@users.noreply.github.com>
2024-02-22 06:37:12 +0000

    Enhanced User class in models.py by incorporating password_hash column with a length of 100, implementing a hash algorithm for secure password storage.

--- market/models.py
+++ market/models.py
@@ -4,6 +4,7 @@ class User(db.Model):
   id = db.Column(db.Integer(), primary_key=True)
   username = db.Column(db.String(length=100), nullable=False)
   email_address = db.Column(db.String(length=100), nullable=False, unique = True)
+  password_hash = db.Column(db.String(length=100), nullable=False)
   
 
 class Item(db.Model):
