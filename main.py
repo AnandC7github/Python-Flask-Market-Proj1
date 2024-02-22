56e5b26865990601435fdcc885d5e8a4e20f2e1e
Anand C <122742595+AnandC7github@users.noreply.github.com>
2024-02-22 09:34:48 +0000

    removed a duplicate item using id

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 from market import app, db
 from market.models import Item, User
 
+def remove_item_by_id(item_id):
+    item_to_remove = Item.query.get(item_id)
+    if item_to_remove:
+        db.session.delete(item_to_remove)
+        db.session.commit()
+        print(f"Item with ID {item_id} successfully removed.")
+    else:
+        print(f"Item with ID {item_id} not found.")
 
-#checks if the run.py has executed directly and not imported 
 if __name__ == '__main__':
-    print(__name__)
-    app.run(host="0.0.0.0", port=3000, debug=True)
+    with app.app_context():
+        remove_item_by_id(2)
+        print(__name__)
+        app.run(host="0.0.0.0", port=3000, debug=True)
