5e5bc5bcb5e30489a604b6028d7429f12d2060eb
Anand C <122742595+AnandC7github@users.noreply.github.com>
2024-02-22 10:07:34 +0000

    modification made

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from market import app, db
 from market.models import Item, User
 
-
 if __name__ == '__main__':
-      print(__name__)
-      app.run(host="0.0.0.0", port=3000, debug=True)
+    with app.app_context():
+        item_to_update = Item.query.get(2)
+
+        items_to_delete = Item.query.filter(Item.id.between(2, 14)).all()
+        for item in items_to_delete:
+            db.session.delete(item)
+        db.session.commit()
+
+        print(__name__)
+        app.run(host="0.0.0.0", port=3000, debug=True)
