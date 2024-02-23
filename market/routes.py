7c3d8ca31ad98f24c0ee47ef7b68a5ea9b24e7e3
Anand C <122742595+AnandC7github@users.noreply.github.com>
2024-02-23 06:53:34 +0000

    Added user creation to the database in register route

--- market/routes.py
+++ market/routes.py
@@ -20,6 +20,6 @@ def register_page():
   form = RegisterForm()
   if form.validate_on_submit():
     user_to_create = User(username=form.username.data, email_address=form.email_address.data, password = form.password1.data)
-    
+    db.session.add(user_to_create)
     return render_template('register.html', form=form)
 
