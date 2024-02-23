957e01f065c002862b8aadad2cd5b9af4d34176d
Anand C <122742595+AnandC7github@users.noreply.github.com>
2024-02-23 08:56:41 +0000

    Added email validation to the RegisterForm in forms.py by using the Email() validator from wtforms.validators

--- market/forms.py
+++ market/forms.py
@@ -4,7 +4,7 @@ from wtforms.validators import  Length, EqualTo, Email
 
 class RegisterForm(FlaskForm):
   username = StringField(label = 'User Name:',validators = [Length(min = 2, max = 50)])
-  email_address = StringField(label = 'Email Address:')
+  email_address = StringField(label = 'Email Address:', validators = Email())
   password1 = PasswordField(label = 'Password:', validators = [Length(min=6)])
   password2 = PasswordField(label = 'Confirm Password:', validators = [EqualTo('password1')])
   submit = SubmitField(label = 'Create Account')
