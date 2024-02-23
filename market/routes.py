57ec52aecf610ccffd84ec346ab90161d05aca6b
Anand C <122742595+AnandC7github@users.noreply.github.com>
2024-02-23 07:01:05 +0000

    Updated the register_page route to accept both GET and POST requests by adding the 'methods' parameter and specifying 'GET' and 'POST' as valid methods.

--- market/routes.py
+++ market/routes.py
@@ -15,7 +15,7 @@ def market_page():
   items = Item.query.all()
   return render_template('market.html', items=items)
 
-@app.route('/register')
+@app.route('/register', methods = ['GET','POST'])
 def register_page():
   form = RegisterForm()
   if form.validate_on_submit():
