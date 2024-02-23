dce076e6dee702f30b01829897f200c7db02e82c
Anand C <122742595+AnandC7github@users.noreply.github.com>
2024-02-23 07:15:17 +0000

    Added length validation for 'username' and 'password1' fields in forms.py to ensure user input meets specified length requirements.

--- market/forms.py
+++ market/forms.py
@@ -3,8 +3,8 @@ from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import  Length
 
 class RegisterForm(FlaskForm):
-  username = StringField(label = 'User Name:')
+  username = StringField(label = 'User Name:',validators = Length(min = 2, max = 50))
   email_address = StringField(label = 'Email Address:')
-  password1 = PasswordField(label = 'Password:')
+  password1 = PasswordField(label = 'Password:', validators = Length(min=6))
   password2 = PasswordField(label = 'Confirm Password:')
   submit = SubmitField(label = 'Create Account')
