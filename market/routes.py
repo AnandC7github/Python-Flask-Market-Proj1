748cdcdd2c09ce25a020bd8e9152c4f6e4e5c70c
Anand C <122742595+AnandC7github@users.noreply.github.com>
2024-02-23 09:11:15 +0000

    a built in function flash has been imported

--- market/routes.py
+++ market/routes.py
@@ -1,6 +1,6 @@
 from wtforms.fields.simple import PasswordField
 from market import app
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, flash
 from market.models import Item, User
 from market.forms import RegisterForm
 from market import db
