86cc464e547a84ceeffba0f337faa46455a4293b
Anand C <122742595+AnandC7github@users.noreply.github.com>
2024-02-26 09:10:01 +0000

    Integrate Flask-Bcrypt for password hashing
    In the `__init__.py` file, added the necessary configurations to integrate Flask-Bcrypt for password hashing in the Flask application. Also, created an instance of Flask-Bcrypt to be used for password-related functionalities.
    
    This enhancement ensures secure password handling with bcrypt in the application, enhancing overall security for user credentials.

--- market/__init__.py
+++ market/__init__.py
@@ -6,5 +6,5 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
 app.config['SECRET_KEY'] = 'b5d9ed02db8471d660ba4e35'
 db = SQLAlchemy(app)
-
+bcrypt = Bcrypt(app) # creating an instance
 from market import routes
