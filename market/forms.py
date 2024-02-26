a24b651b1a79f560e2895ac47353f7438aba63bc
Anand C <122742595+AnandC7github@users.noreply.github.com>
2024-02-26 06:13:18 +0000

    Add email address validation in user registration form
    In the `RegisterForm` class of the `forms.py` file, implemented email address validation to check if the entered email address already exists in the database. If an existing email address is found, a `ValidationError` is raised, providing a helpful error message to the user during registration.
    
    This enhancement ensures that users cannot register with duplicate email addresses, improving the integrity of the user database.

--- market/forms.py
+++ market/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import  Length, EqualTo, Email, DataRequired
+from wtforms.validators import  Length, EqualTo, Email, DataRequired, ValidationError
 from market.models import User
 
 class RegisterForm(FlaskForm):
@@ -8,6 +8,12 @@ class RegisterForm(FlaskForm):
   #   user = User.query.filter_by(username=username_to_check.data).first()
   #   if user:
   #     raise DataRequired('Username already exists! Please try a different username')
+
+  def validate_email_address(self, email_address_to_check):
+    email_address = User.query.filter_by(email_address=email_address_to_check).first()
+    if email_address:
+      raise ValidationError('Email address already exists! Please try a different email address')
+      
   username = StringField(label = 'User Name:',validators = [Length(min = 2, max = 50), DataRequired()])
   email_address = StringField(label = 'Email Address:', validators = [Email(), DataRequired()])
   password1 = PasswordField(label = 'Password:', validators = [Length(min=6), DataRequired()])
