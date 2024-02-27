4c1f0d31e1045d6f0ee14e21b70fb13fbdd19ba9
Ajay <59962924+Ajay-user@users.noreply.github.com>
2024-02-27 12:09:07 +0000

    Fix login page issues

--- market/routes.py
+++ market/routes.py
@@ -1,45 +1,59 @@
 from market import app
-from flask import render_template, redirect, url_for, flash
+from flask import render_template, redirect, url_for, flash, request
 from market.models import Item, User
 from market.forms import RegisterForm, LoginForm
-from market import db
+from market import db, bcrypt
 from flask_login import login_user
+
 
 @app.route('/')
 @app.route('/home')
 def home_page():
-    return render_template('home.html')
+  return render_template('home.html')
+
 
 @app.route('/market')
 def market_page():
-    items = Item.query.all()
-    return render_template('market.html', items=items)
+  items = Item.query.all()
+  return render_template('market.html', items=items)
+
 
 @app.route('/register', methods=['GET', 'POST'])
 def register_page():
-    form = RegisterForm()
-    if form.validate_on_submit():
-        user_to_create = User(username=form.username.data,email_address=form.email_address.data,password=form.password1.data)
-        db.session.add(user_to_create)
-        db.session.commit()
-        return redirect(url_for('market_page'))
-    if form.errors != {}: #If there are not errors from the validations
-          for err_msg in form.errors.values():
-            flash(f'Error: {err_msg[0]}', 'danger')
-            print(f'Error: {err_msg[0]}')
+  form = RegisterForm()
+  if form.validate_on_submit():
+    password_hash = bcrypt.generate_password_hash(
+        form.password1.data.encode('utf-8'))
+    user_to_create = User(username=form.username.data,
+                          email_address=form.email_address.data,
+                          password_hash=password_hash)
+    db.session.add(user_to_create)
+    db.session.commit()
+    return redirect(url_for('login_page'))
+  if form.errors != {}:  #If there are not errors from the validations
+    for err_msg in form.errors.values():
+      flash(f'Error: {err_msg[0]}', 'danger')
+      print(f'Error: {err_msg[0]}')
 
-    return render_template('register.html', form=form)
+  return render_template('register.html', form=form)
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login_page():
   form = LoginForm()
-  if form.validate_on_submit():
-    attempted_user = User.query.filter_by(username = form.username.data).first()
-    if attempted_user and attempted_user.check_password_correction(attempted_password=form.password.data):
+
+  if form.validate_on_submit() and request.method == "POST":
+    attempted_user = User.query.filter_by(
+        email_address=form.email.data).first()
+    if attempted_user and bcrypt.check_password_hash(
+        attempted_user.password_hash, form.password.data):
+
       login_user(attempted_user)
-      flash(f'Success! You are logged in as {attempted_user.username}', category = 'success')
+      flash(f'Success! You are logged in as {attempted_user.username}',
+            category='success')
       return redirect(url_for('market_page'))
     else:
-      flash('Username and password incorrect! Please try again!', category='danger')
+      flash('Username and password incorrect! Please try again!',
+            category='danger')
+
   return render_template('login.html', form=form)
