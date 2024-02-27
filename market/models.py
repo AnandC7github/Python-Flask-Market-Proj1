e81b0533146201f5e7a6c025cc73cbf34e838551
Anand C <122742595+AnandC7github@users.noreply.github.com>
2024-02-27 06:07:28 +0000

    Added UserMixin to the User class for Flask-Login compatibility.

--- market/models.py
+++ market/models.py
@@ -6,7 +6,7 @@ from flask_login import UserMixin
 def load_user(user_id):
     return User.query.get(int(user_id))
 
-class User(db.Model):
+class User(db.Model, UserMixin):
   id = db.Column(db.Integer(), primary_key=True)
   username = db.Column(db.String(length=100), nullable=False)
   email_address = db.Column(db.String(length=100), nullable=False, unique = True)
