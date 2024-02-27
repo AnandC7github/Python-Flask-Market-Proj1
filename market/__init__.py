912796afd117531d0a3269705d4ab51687997d9b
Anand C <122742595+AnandC7github@users.noreply.github.com>
2024-02-27 05:13:04 +0000

    Implemented 'login_manager = LoginManager(app)' for user authentication in the application.

--- market/__init__.py
+++ market/__init__.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
-from flask_login import LoginManager
+from flask_login import LoginManager, login_manager
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
 app.config['SECRET_KEY'] = 'b5d9ed02db8471d660ba4e35'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app) # creating an instance
+login_manager = LoginManager(app)
 from market import routes
