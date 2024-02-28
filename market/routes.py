d2908451f0260ad200e4add6a077b3bda3e1a52b
Anand C <122742595+AnandC7github@users.noreply.github.com>
2024-02-28 06:49:07 +0000

    imported login_required from flask_login

--- market/routes.py
+++ market/routes.py
@@ -3,7 +3,7 @@ from flask import render_template, redirect, url_for, flash, request
 from market.models import Item, User
 from market.forms import RegisterForm, LoginForm
 from market import db, bcrypt
-from flask_login import login_user, logout_user
+from flask_login import login_user, logout_user, login_required
 
 
 @app.route('/')
