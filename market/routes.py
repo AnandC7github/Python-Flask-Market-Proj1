db8352ec3ca571f508df4dc7889848fd81783f3a
Anand C <122742595+AnandC7github@users.noreply.github.com>
2024-02-29 09:58:00 +0000

    Enhance market_page with purchase success flash message
    In routes.py, added a flash message to notify the user with a congratulatory message upon successful purchase of an item. This provides feedback about the purchase, displaying the item name and its price.

--- market/routes.py
+++ market/routes.py
@@ -25,6 +25,7 @@ def market_page():
       p_item_object.owner = current_user.id
       current_user.budget -= p_item_object.price
       db.session.commit()
+      flash(f'Congratulations! You have purchased {p_item_object.name} for ${p_item_object.price}.')
   items = Item.query.filter_by(owner = None)
   return render_template('market.html', items=items, purchase_form = purchase_form)
 
