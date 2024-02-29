5a0eb95f4cb486589ffe17953571d3864eafd72f
Anand C <122742595+AnandC7github@users.noreply.github.com>
2024-02-29 09:22:56 +0000

    Update print statement to access 'purchased_item' field
    In routes.py, changed the print statement from accessing the 'submit' field to 'purchased_item' field in the PurchaseItemForm, reflecting the adjustment in form field usage.

--- market/routes.py
+++ market/routes.py
@@ -17,7 +17,7 @@ def home_page():
 def market_page():
   purchase_form = PurchaseItemForm()
   if purchase_form.validate_on_submit():
-    print(purchase_form['submit'])
+    print(purchase_form['purchased_item'])
   items = Item.query.all()
   return render_template('market.html', items=items, purchase_form = purchase_form)
 
