697e924b78832dfe65e43393c4be86e9e12fa8a7
Anand C <122742595+AnandC7github@users.noreply.github.com>
2024-02-29 09:08:33 +0000

    Print dictionary representation of PurchaseItemForm
    In routes.py, modified the print statement for purchase_form to print its __dict__ attribute, providing a detailed view of the form's properties.

--- market/routes.py
+++ market/routes.py
@@ -17,7 +17,7 @@ def home_page():
 def market_page():
   purchase_form = PurchaseItemForm()
   if purchase_form.validate_on_submit():
-    print(purchase_form)
+    print(purchase_form.__dict__)
   items = Item.query.all()
   return render_template('market.html', items=items, purchase_form = purchase_form)
 
