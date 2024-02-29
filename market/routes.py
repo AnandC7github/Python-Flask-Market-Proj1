ca8e73bbd8bd8d9981be0c1640e2bcef4d75cf1e
Anand C <122742595+AnandC7github@users.noreply.github.com>
2024-02-29 11:40:58 +0000

    Enhance market_page with owned items and owner parameter
    In routes.py, further improved the market_page route by adding the 'owner' parameter to the render_template function. This addition ensures that the 'owner' variable is available in the template, providing flexibility for rendering items based on ownership. The change enhances the clarity and extensibility of the market page.

--- market/routes.py
+++ market/routes.py
@@ -34,7 +34,7 @@ def market_page():
   if request.method == 'GET':
     items = Item.query.filter_by(owner = None)
     owned_items = Item.query.filter_by(owner = current_user.id)
-  return render_template('market.html', items=items, purchase_form = purchase_form)
+  return render_template('market.html', items=items, purchase_form = purchase_form, owner = owner)
 
 
 @app.route('/register', methods=['GET', 'POST'])
