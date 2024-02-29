e0534a1b155c666ea29e6004f11509a263a49748
Anand C <122742595+AnandC7github@users.noreply.github.com>
2024-02-29 09:27:04 +0000

    Update print statement to print entire request form data
    In routes.py, changed the print statement from printing a specific field (purchase_form['purchased_item']) to printing the entire request form using request.form. Also, imported 'request' from Flask to access form data.

--- market/routes.py
+++ market/routes.py
@@ -17,7 +17,7 @@ def home_page():
 def market_page():
   purchase_form = PurchaseItemForm()
   if purchase_form.validate_on_submit():
-    print(purchase_form['purchased_item'])
+    print(request.form)
   items = Item.query.all()
   return render_template('market.html', items=items, purchase_form = purchase_form)
 
