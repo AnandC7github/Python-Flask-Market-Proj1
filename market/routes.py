f0ad279630dca15acc9c735b8478c08678981ff6
Anand C <122742595+AnandC7github@users.noreply.github.com>
2024-03-01 05:42:21 +0000

    imported SellItemForm from market.forms

--- market/routes.py
+++ market/routes.py
@@ -1,7 +1,7 @@
 from market import app
 from flask import render_template, redirect, url_for, flash, request
 from market.models import Item, User
-from market.forms import RegisterForm, LoginForm, PurchaseItemForm
+from market.forms import RegisterForm, LoginForm, PurchaseItemForm, SellItemForm
 from market import db, bcrypt
 from flask_login import login_user, logout_user, login_required, current_user
 
